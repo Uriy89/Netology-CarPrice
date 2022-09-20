c94e79f65d173824f29156c91bb173876e279496
Юрий Аверьянов <averianovj@gmail.com>
2022-09-20 21:29:47 +0300

    correction HW

--- src/main/java/task/CarShop.java
+++ src/main/java/task/CarShop.java
@@ -1,8 +1,13 @@
 package task;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CarShop {
     private final Producer producer;
-    private final int TIMEOUT = 2000;
+    private final static int TIMEOUT = 2000;
+
+    private List<Car> cars = new ArrayList<>();
     private boolean isCar = false;
     private String car = "";
 
@@ -11,8 +16,9 @@ public class CarShop {
     }
 
     public synchronized void buyCar() {
-        while (producer.isNextCar()){
+        do {
             System.out.println(Thread.currentThread().getName() + " прибыл в салон");
+            Producer.setListCars(cars);
             try {
                 while (!isCar){
                     System.out.println("Машин нет!");
@@ -28,24 +34,39 @@ public class CarShop {
             System.out.println(Thread.currentThread().getName() + " поехал на новеньком " + car);
             try {
                 Thread.sleep(TIMEOUT);
-            } catch (InterruptedException e) {}
-        }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        } while (isNextCar());
     }
 
     public synchronized void putCar() {
-        while (producer.isNextCar()){
+        while (isNextCar()){
             if (!isCar){
-                car = producer.getNextCar();
+                car = getNextCar();
                 isCar = true;
-                System.out.println("\nПроизводитель " + car + " выпустил 1 авто\n");
+                try {
+                    System.out.println("\nПроизводитель " + car + " выпустил 1 авто\n");
+                } catch (NullPointerException e) {
+                    e.printStackTrace();
+                }
                 notifyAll();
             }
             try {
-                wait(producer.INTERVAL);
+                wait(Producer.INTERVAL);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
         Thread.currentThread().interrupt();
+    }
+
+
+    private String getNextCar() {
+        return isNextCar() ? cars.remove(0).getName() : null;
+    }
+
+    private boolean isNextCar() {
+        return cars.size() > 0;
     }
 }
