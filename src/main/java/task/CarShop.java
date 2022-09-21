053af7e3299ba880208afc00cf4917b51aca0b8e
Юрий Аверьянов <averianovj@gmail.com>
2022-09-21 21:09:56 +0300

    refact HW

--- src/main/java/task/CarShop.java
+++ src/main/java/task/CarShop.java
@@ -1,51 +1,59 @@
 package task;
 
-public class CarShop {
-    private final Producer producer;
-    private final int TIMEOUT = 2000;
-    private boolean isCar = false;
-    private String car = "";
+import java.util.ArrayList;
+import java.util.List;
 
-    public CarShop() {
-        this.producer = new Producer();
-    }
+public class CarShop extends Thread {
 
+    private final List<Car> cars = new ArrayList<>();
+    private static final int TIMEOUT = 3000;
+    private static final int CREATE = 1000;
+    private static  int COUNT = 10;
     public synchronized void buyCar() {
-        while (producer.isNextCar()){
-            System.out.println(Thread.currentThread().getName() + " прибыл в салон");
-            try {
-                while (!isCar){
-                    System.out.println("Машин нет!");
+        System.out.println(Thread.currentThread().getName() + " прибыл в салон");
+        while (COUNT > 0) {
+            if (cars.size() == 0) {
+                System.out.println("Машин нет!");
+                try {
                     wait();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
                 }
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+
+                try {
+                    Thread.sleep(TIMEOUT);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            } else {
+                try {
+                    Thread.sleep(TIMEOUT);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println(Thread.currentThread().getName() + " поехал на новеньком авто");
+                COUNT--;
+                cars.remove(0);
             }
-            isCar = false;
-            try {
-                Thread.sleep(TIMEOUT);
-            } catch (InterruptedException e) {}
-            System.out.println(Thread.currentThread().getName() + " поехал на новеньком " + car);
-            try {
-                Thread.sleep(TIMEOUT);
-            } catch (InterruptedException e) {}
         }
     }
 
-    public synchronized void putCar() {
-        while (producer.isNextCar()){
-            if (!isCar){
-                car = producer.getNextCar();
-                isCar = true;
-                System.out.println("\nПроизводитель " + car + " выпустил 1 авто\n");
+
+    public void produceCar() {
+        while (COUNT > 0) {
+            try {
+                Thread.sleep(CREATE);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            cars.add(new Car());
+            String name = cars.get(0).getName();
+            synchronized (this) {
+                System.out.println("\nПроизводитель " + name + " выпустил 1 авто\n");
                 notifyAll();
             }
-            try {
-                wait(producer.INTERVAL);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+
         }
-        Thread.currentThread().interrupt();
     }
+
 }
--- src/main/java/task/Main.java
+++ src/main/java/task/Main.java
@@ -4,21 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
         final CarShop shop = new CarShop();
+
         Runnable shopping = shop::buyCar;
         Thread bayer1 = new Thread(null, shopping, "Покупатель1");
-        bayer1.setDaemon(true);
         bayer1.start();
         Thread bayer2 = new Thread(null, shopping, "Покупатель2");
-        bayer2.setDaemon(true);
         bayer2.start();
         Thread bayer3 = new Thread(null, shopping, "Покупатель3");
-        bayer3.setDaemon(true);
         bayer3.start();
         Thread bayer4 = new Thread(null, shopping, "Покупатель4");
-        bayer4.setDaemon(true);
         bayer4.start();
 
-        new Thread(null, shop::putCar, "Автопром").start();
+        new Thread(null, shop::produceCar, "Автопром").start();
     }
 
 
